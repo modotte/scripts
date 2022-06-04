c564011a7545e885c0a55d286524b5204e078776
modotte <runecroon@gmail.com>
2022-06-04 20:29:24 +0800

    Clean up

--- gif.sh
+++ gif.sh
@@ -11,11 +11,16 @@ if [[ ! $(command -v jq) || ! $(command -v curl) ]]; then
     for i in "${required_tools[@]}"; do
         test ! "$(command -v "$i")" && >&2 echo "You are missing the $i tool!"
     done
-    >&2 echo "Need both jq and curl tool installed to run!"
+    >&2 echo "Need both jq and curl tools installed to run!"
     exit 1
 fi
 
 API_HEADER="Accept: application/vnd.github.v3+json"
+
+show_help () {
+    >&2 echo "Usage: $0 [--language='haskell'|--label='good first issue']"
+    >&2 echo "Example: $0"
+}
 
 LANGUAGE="$1"
 
@@ -23,8 +28,8 @@ data=$(curl -s -H "$API_HEADER" "https://api.github.com/search/issues?q=language
 
 titles=()
 html_urls=()
-readarray -t titles < <(echo "$data" | jq '. | .items | .[].title')
-readarray -t html_urls < <(echo "$data" | jq '. | .items | .[].html_url')
+readarray -t titles < <(echo "$data" | jq '.items.[].title')
+readarray -t html_urls < <(echo "$data" | jq '.items.[].html_url')
 
 for i in "${!titles[@]}"; do
     echo "Title: ${titles[$i]}"
